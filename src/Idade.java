0bdc9231458aecefc3b2ffdb77e16957db51be08
MichelecFernandes <michelecristinafr@gmail.com>
2024-02-08 10:28:46 -0300

    Enviando os primeiros arquivos

---
+++ src/Idade.java
@@ -0,0 +1,41 @@
+public class Idade {
+    private int idade;
+
+    public Idade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void exibirIdade(){
+        System.out.println("A idade é de " + getIdade() + " anos. ");
+    }
+
+    public int somaMultiplicaIdade(int soma, int multiplica){
+        this.idade = (this.idade + soma) * multiplica;
+        return this.idade;
+    }
+
+    public int subtraiIdade(int subtrai){
+        this.idade -= subtrai;
+        return this.idade;
+    }
+
+    public void verificaIdade(int comparaIdade){
+        if (this.idade > comparaIdade){
+            System.out.println("A idade é maior que  " + comparaIdade + ".");
+        }else {
+            System.out.println("A idade é menor que  " + comparaIdade + ".");
+        }
+    }
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Idade idadeDeclarada = new Idade(15);
+        idadeDeclarada.exibirIdade();
+        System.out.println("Resultao 1: " + idadeDeclarada.somaMultiplicaIdade(5,2) + " . ");
+        System.out.println("Resultao 2: " + idadeDeclarada.subtraiIdade(5) + " . ");
+        idadeDeclarada.verificaIdade(20);
+
+    }
+
+}
